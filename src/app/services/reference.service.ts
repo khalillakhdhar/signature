c1f92807b86b9745e46192ac2232617b74d98b89
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-10 12:06:08 +0100

    Update reference.service.ts

--- src/app/services/reference.service.ts
+++ src/app/services/reference.service.ts
@@ -1,9 +1,30 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReferenceService {
 
-  constructor() { }
+  constructor(
+    private firestore: AngularFirestore
+  ) { }
+
+
+  create_NewReference(record) {
+    return this.firestore.collection('References').add(record);
+  }
+
+  read_References() {
+    return this.firestore.collection('References').snapshotChanges();
+  }
+
+  update_Reference(recordID, record) {
+    this.firestore.doc('References/' + recordID).update(record);
+    console.log('updated');
+  }
+
+  delete_Reference(record_id) {
+    this.firestore.doc('References/' + record_id).delete();
+  }
 }

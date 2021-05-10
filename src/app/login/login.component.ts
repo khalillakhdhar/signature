2f681b4f7fdded325722de7a6ff103ae888acad8
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-10 12:24:30 +0100

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,54 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+user:User;
+users:User[];
+correcte=false;
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.user=new User();
+    this.read();
   }
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
 
+      this.users = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+        
+         nom: e.payload.doc.data()["nom"],
+         prenom: e.payload.doc.data()["prenom"],
+         email: e.payload.doc.data()["email"],
+         mdp: e.payload.doc.data()["mdp"],
+         rib: e.payload.doc.data()["rib"],
+         cin: e.payload.doc.data()["cin"],
+         grade: e.payload.doc.data()["grade"],
+         adresse: e.payload.doc.data()["adresse"],
+         age: e.payload.doc.data()["age"],
+  
+  
+  
+        };
+      });
+      console.log("users",this.users);
+    });
+  }
+  verif()
+  {
+    for(let us of this.users)
+    {
+      if((us.email==this.user.email)&&(us.mdp==this.user.mdp))
+      {
+        this.correcte=true;
+        window.location.replace("profile");
+      }
+
+
+    }
+    if(!this.correcte)
+    alert("email ou mot de passe incorrectes")
+  }
 }

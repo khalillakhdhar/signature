3992c24eedd4090e0e78c28f34bee94126fe75d2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-10 12:39:55 +0100

    Update profile.component.ts

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../classes/user';
+import { UserService } from '../services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -6,10 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./profile.component.css']
 })
 export class ProfileComponent implements OnInit {
-
-  constructor() { }
+user:User;
+users:User[];
+id:string;
+  constructor(private userService:UserService) { }
 
   ngOnInit(): void {
+    this.id=localStorage.getItem("id");
+    this.user=new User();
+    this.users=[];
+    this.read();
   }
+  read()
+  {
+    this.userService.read_Users().subscribe(data => {
 
+      this.users = data.map(e => {
+        return {
+         id: e.payload.doc.id,
+  
+        
+         nom: e.payload.doc.data()["nom"],
+         prenom: e.payload.doc.data()["prenom"],
+         email: e.payload.doc.data()["email"],
+         mdp: e.payload.doc.data()["mdp"],
+         rib: e.payload.doc.data()["rib"],
+         cin: e.payload.doc.data()["cin"],
+         grade: e.payload.doc.data()["grade"],
+         adresse: e.payload.doc.data()["adresse"],
+         age: e.payload.doc.data()["age"],
+  
+  
+  
+        };
+      });
+      console.log("users",this.users);
+      this.getprofile();
+    });
+   
+
+  }
+  getprofile()
+  {
+    for(let us of this.users)
+    {
+      if(this.id==us.id)
+      this.user=us;
+    }
+    console.log("current",this.user);
+
+  }
+  update()
+  {}
 }
